4c30a4bf513dc163bd74f9e270455be8ff56d352
Maël Pedretti <mael.pedretti@he-arc.ch>
2017-01-16 14:18:13 +0100

    modification des commentaires de main.cpp

--- Labo2/main.cpp
+++ Labo2/main.cpp
@@ -2,6 +2,7 @@
  *                  Labo2
  *                 Laurent Gander et Maël Pedretti
  *               HE-Arc 2017
+ *
  */
 
 #include "mainwindow.h"
@@ -24,7 +25,12 @@ int main(int argc, char *argv[])
     //nbBorne = 5;
     //nbVelo = 20;
 
-    if(nbSite >= 2 && nbBorne >= 4 && nbVelo >= nbSite*(nbBorne-2)+3)
+    //grace a l'utilisation de la fonction atoi(), si le paramètre entré n'est pas un nombre, 0 est retourné
+    //il suffit donc de tester si les paramètres sont supérieurs à 0 et s'ils correspondent aux paramètres
+    //requis pour le bon fonctionnement du programme
+    //si ce n'est pas le cas, un erreur est affichée
+
+    if(nbSite >= 2 && nbBorne >= 4 && nbVelo >= nbSite*(nbBorne-2)+3 && nbHabitants > 0)
     {
         MainWindow* mainWindow = new MainWindow(nbSite,nbHabitants,nbBorne,nbVelo);
         mainWindow->show();
@@ -35,7 +41,11 @@ int main(int argc, char *argv[])
         return a.exec();
     }
     else{
-        std::cout << "nbSite " << nbSite << " nbHabitants " << nbHabitants << " nbBorne " << nbBorne << " nbVelo " << nbVelo << std::endl;
+        std::cout << "un ou plusieurs parametres entres sont invalides :"<<std::endl
+                  << "le nombre de site doit etre superieur a 2. Valeur entree : " << nbSite << std::endl
+                  << "le nombre d'habitants doit etre superieur a 0. Valeur entree : " << nbHabitants << std::endl
+                  << "le nombre de bornes doit etre superieur a 4. Valeur entree : " << nbBorne << std::endl
+                  << "le nombre de velos doit etre superieur a nbSite*(nbBorne-2)+3. valeur entree : "<< nbVelo << std::endl;
         system("pause");
         return 0;
     }
